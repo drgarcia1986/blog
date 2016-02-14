249661423c4e20b9b4e3e7d1090114b3de6a569c
Diego Garcia <drgarcia1986@gmail.com>
2016-02-14 14:22:05 -0200

    update siteurl

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://drgarcia1986.github.io/blog'
+SITEURL = 'http://www.diego-garcia.info'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'atom.xml'
